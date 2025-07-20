aa3b5998eb00fbac9417448c03ca6f95a3d1daed
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2025-07-20 14:33:54 +0200

    Add TVShow and TVShowList models with client methods
    Implemented TVShow and TVShowList models to represent TV show data
    and added methods in Client to fetch TV shows asynchronously.
    Included test cases for validation.

---
+++ Tests/TMDBTests/TVShows/ClientTVShowTests.swift
@@ -0,0 +1,40 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+import Testing
+import TMDB
+
+struct ClientTVShowTests {
+    @Test(
+        arguments: [
+            (TVShowList.airingToday, "https://api.themoviedb.org/3/tv/airing_today"),
+            (TVShowList.onTheAir, "https://api.themoviedb.org/3/tv/on_the_air"),
+            (TVShowList.popular, "https://api.themoviedb.org/3/tv/popular"),
+            (TVShowList.topRated, "https://api.themoviedb.org/3/tv/top_rated"),
+        ]
+    )
+    func tvShowsSuccess(list: TVShowList, absoluteURLString: String) async throws {
+        // Setup
+        let urlRequestStorage = TestStorage<URLRequest>()
+        // Test
+        let client = Client(accessToken: "ABC123") {
+            await urlRequestStorage.setValue($0)
+            return Response(data: .tvShowPageContent, statusCode: 200)
+        }
+        await #expect(throws: Never.self) {
+            try await client.tvShows(list: list)
+        }
+        // Verify
+        let urlRquest = try #require(await urlRequestStorage.value)
+        #expect(urlRquest.url?.absoluteString == absoluteURLString)
+        #expect(urlRquest.httpMethod == "GET")
+        #expect(urlRquest.httpBody == nil)
+        #expect(
+            urlRquest.allHTTPHeaderFields == [
+                "Authorization": "Bearer ABC123",
+                "Accept": "application/json",
+            ]
+        )
+    }
+}
