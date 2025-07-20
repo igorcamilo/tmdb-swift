1856aeac97fa7644c63c7177481511d5be70b2b7
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2025-07-20 14:40:22 +0200

    Add failure response and request tests for TV shows
    Implemented tests for handling failure responses and requests in the
    ClientTVShowTests. This includes verifying the correct URL, HTTP method,
    and headers for various TV show lists.

--- Tests/TMDBTests/Movies/ClientMovieTests.swift
+++ Tests/TMDBTests/Movies/ClientMovieTests.swift
@@ -37,4 +37,69 @@ struct ClientMovieTests {
             ]
         )
     }
+
+    @Test(
+        arguments: [
+            (MovieList.nowPlaying, "https://api.themoviedb.org/3/movie/now_playing"),
+            (MovieList.popular, "https://api.themoviedb.org/3/movie/popular"),
+            (MovieList.topRated, "https://api.themoviedb.org/3/movie/top_rated"),
+            (MovieList.upcoming, "https://api.themoviedb.org/3/movie/upcoming"),
+        ]
+    )
+    func moviesFailureResponse(list: MovieList, absoluteURLString: String) async throws {
+        // Setup
+        let urlRequestStorage = TestStorage<URLRequest>()
+        // Test
+        let client = Client(accessToken: "DEF456") {
+            await urlRequestStorage.setValue($0)
+            return Response(data: .errorContent, statusCode: 400)
+        }
+        await #expect(throws: ErrorContent.self) {
+            try await client.movies(list: list)
+        }
+        // Verify
+        let urlRquest = try #require(await urlRequestStorage.value)
+        #expect(urlRquest.url?.absoluteString == absoluteURLString)
+        #expect(urlRquest.httpMethod == "GET")
+        #expect(urlRquest.httpBody == nil)
+        #expect(
+            urlRquest.allHTTPHeaderFields == [
+                "Authorization": "Bearer DEF456",
+                "Accept": "application/json",
+            ]
+        )
+    }
+
+    @Test(
+        arguments: [
+            (MovieList.nowPlaying, "https://api.themoviedb.org/3/movie/now_playing"),
+            (MovieList.popular, "https://api.themoviedb.org/3/movie/popular"),
+            (MovieList.topRated, "https://api.themoviedb.org/3/movie/top_rated"),
+            (MovieList.upcoming, "https://api.themoviedb.org/3/movie/upcoming"),
+        ]
+    )
+    func moviesFailureRequest(list: MovieList, absoluteURLString: String) async throws {
+        // Setup
+        struct TestError: Error {}
+        let urlRequestStorage = TestStorage<URLRequest>()
+        // Test
+        let client = Client(accessToken: "GHI789") {
+            await urlRequestStorage.setValue($0)
+            throw TestError()
+        }
+        await #expect(throws: TestError.self) {
+            try await client.movies(list: list)
+        }
+        // Verify
+        let urlRquest = try #require(await urlRequestStorage.value)
+        #expect(urlRquest.url?.absoluteString == absoluteURLString)
+        #expect(urlRquest.httpMethod == "GET")
+        #expect(urlRquest.httpBody == nil)
+        #expect(
+            urlRquest.allHTTPHeaderFields == [
+                "Authorization": "Bearer GHI789",
+                "Accept": "application/json",
+            ]
+        )
+    }
 }
--- Tests/TMDBTests/TVShows/ClientTVShowTests.swift
+++ Tests/TMDBTests/TVShows/ClientTVShowTests.swift
@@ -37,4 +37,69 @@ struct ClientTVShowTests {
             ]
         )
     }
+
+    @Test(
+        arguments: [
+            (TVShowList.airingToday, "https://api.themoviedb.org/3/tv/airing_today"),
+            (TVShowList.onTheAir, "https://api.themoviedb.org/3/tv/on_the_air"),
+            (TVShowList.popular, "https://api.themoviedb.org/3/tv/popular"),
+            (TVShowList.topRated, "https://api.themoviedb.org/3/tv/top_rated"),
+        ]
+    )
+    func tvShowsFailureResponse(list: TVShowList, absoluteURLString: String) async throws {
+        // Setup
+        let urlRequestStorage = TestStorage<URLRequest>()
+        // Test
+        let client = Client(accessToken: "DEF456") {
+            await urlRequestStorage.setValue($0)
+            return Response(data: .errorContent, statusCode: 400)
+        }
+        await #expect(throws: ErrorContent.self) {
+            try await client.tvShows(list: list)
+        }
+        // Verify
+        let urlRquest = try #require(await urlRequestStorage.value)
+        #expect(urlRquest.url?.absoluteString == absoluteURLString)
+        #expect(urlRquest.httpMethod == "GET")
+        #expect(urlRquest.httpBody == nil)
+        #expect(
+            urlRquest.allHTTPHeaderFields == [
+                "Authorization": "Bearer DEF456",
+                "Accept": "application/json",
+            ]
+        )
+    }
+
+    @Test(
+        arguments: [
+            (TVShowList.airingToday, "https://api.themoviedb.org/3/tv/airing_today"),
+            (TVShowList.onTheAir, "https://api.themoviedb.org/3/tv/on_the_air"),
+            (TVShowList.popular, "https://api.themoviedb.org/3/tv/popular"),
+            (TVShowList.topRated, "https://api.themoviedb.org/3/tv/top_rated"),
+        ]
+    )
+    func tvShowsFailureRequest(list: TVShowList, absoluteURLString: String) async throws {
+        // Setup
+        struct TestError: Error {}
+        let urlRequestStorage = TestStorage<URLRequest>()
+        // Test
+        let client = Client(accessToken: "GHI789") {
+            await urlRequestStorage.setValue($0)
+            throw TestError()
+        }
+        await #expect(throws: TestError.self) {
+            try await client.tvShows(list: list)
+        }
+        // Verify
+        let urlRquest = try #require(await urlRequestStorage.value)
+        #expect(urlRquest.url?.absoluteString == absoluteURLString)
+        #expect(urlRquest.httpMethod == "GET")
+        #expect(urlRquest.httpBody == nil)
+        #expect(
+            urlRquest.allHTTPHeaderFields == [
+                "Authorization": "Bearer GHI789",
+                "Accept": "application/json",
+            ]
+        )
+    }
 }
