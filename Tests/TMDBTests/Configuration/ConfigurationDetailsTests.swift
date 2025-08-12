c57d08768cdf4859703ff157200b9f566f14544f
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2025-08-12 21:42:01 +0200

    Fix tests

--- Tests/TMDBTests/Configuration/ConfigurationDetailsTests.swift
+++ Tests/TMDBTests/Configuration/ConfigurationDetailsTests.swift
@@ -49,6 +49,37 @@ struct ConfigurationDetailsTests {
 
   @Test(
     arguments: [
+      (100.0, "/logo.png", "https://kinova.igorcamilo.com/w122/logo.png"),
+      (122.001, "/logo2.png", "https://kinova.igorcamilo.com/w123/logo2.png"),
+      (123.0, "/logo3.png", "https://kinova.igorcamilo.com/w123/logo3.png"),
+      (123.999, "/logo4.png", "https://kinova.igorcamilo.com/w124/logo4.png"),
+      (124.0, "/logo5.png", "https://kinova.igorcamilo.com/w124/logo5.png"),
+      (800.0, "/logo6.png", "https://kinova.igorcamilo.com/xyz/logo6.png"),
+      (1000.0, "/logo7.png", "https://kinova.igorcamilo.com/xyz/logo7.png"),
+      (nil, "/logo8.png", "https://kinova.igorcamilo.com/xyz/logo8.png"),
+    ]
+  )
+  func imagesLogoURL(width: Double?, path: LogoPath, expectedURL: String) {
+    var images = testImages
+    images.logoSizes = ["abc", "w122", "h123", "w123", "w124", "xyz"]
+    let url = images.url(width: width, path: path)
+    #expect(url?.absoluteString == expectedURL)
+  }
+
+  @Test func imagesLogoNoURL() {
+    var images = ConfigurationDetails.Images()
+    images.logoSizes = ["abc"]
+    let url = images.url(path: "logo.png" as LogoPath)
+    #expect(url == nil)
+  }
+
+  @Test func imagesLogoNoPath() {
+    let url = testImages.url(path: "logo.svg" as LogoPath)
+    #expect(url == nil)
+  }
+
+  @Test(
+    arguments: [
       (100.0, "/123.jpg", "https://kinova.igorcamilo.com/w122/123.jpg"),
       (122.001, "/456.jpg", "https://kinova.igorcamilo.com/w123/456.jpg"),
       (123.0, "/789.jpg", "https://kinova.igorcamilo.com/w123/789.jpg"),
@@ -83,6 +114,68 @@ struct ConfigurationDetailsTests {
     // Test
     let url = testImages.url(path: "/456.svg" as PosterPath)
     // Verify
+    #expect(url == nil)
+  }
+
+  @Test(
+    arguments: [
+      (100.0, "/profile.png", "https://kinova.igorcamilo.com/w122/profile.png"),
+      (122.001, "/profile2.png", "https://kinova.igorcamilo.com/w123/profile2.png"),
+      (123.0, "/profile3.png", "https://kinova.igorcamilo.com/w123/profile3.png"),
+      (123.999, "/profile4.png", "https://kinova.igorcamilo.com/w124/profile4.png"),
+      (124.0, "/profile5.png", "https://kinova.igorcamilo.com/w124/profile5.png"),
+      (800.0, "/profile6.png", "https://kinova.igorcamilo.com/xyz/profile6.png"),
+      (1000.0, "/profile7.png", "https://kinova.igorcamilo.com/xyz/profile7.png"),
+      (nil, "/profile8.png", "https://kinova.igorcamilo.com/xyz/profile8.png"),
+    ]
+  )
+  func imagesProfileURL(width: Double?, path: ProfilePath, expectedURL: String) {
+    var images = testImages
+    images.profileSizes = ["abc", "w122", "h123", "w123", "w124", "xyz"]
+    let url = images.url(width: width, path: path)
+    #expect(url?.absoluteString == expectedURL)
+  }
+
+  @Test func imagesProfileNoURL() {
+    var images = ConfigurationDetails.Images()
+    images.profileSizes = ["abc"]
+    let url = images.url(path: "profile.png" as ProfilePath)
+    #expect(url == nil)
+  }
+
+  @Test func imagesProfileNoPath() {
+    let url = testImages.url(path: "profile.svg" as ProfilePath)
+    #expect(url == nil)
+  }
+
+  @Test(
+    arguments: [
+      (100.0, "/still.png", "https://kinova.igorcamilo.com/w122/still.png"),
+      (122.001, "/still2.png", "https://kinova.igorcamilo.com/w123/still2.png"),
+      (123.0, "/still3.png", "https://kinova.igorcamilo.com/w123/still3.png"),
+      (123.999, "/still4.png", "https://kinova.igorcamilo.com/w124/still4.png"),
+      (124.0, "/still5.png", "https://kinova.igorcamilo.com/w124/still5.png"),
+      (800.0, "/still6.png", "https://kinova.igorcamilo.com/xyz/still6.png"),
+      (1000.0, "/still7.png", "https://kinova.igorcamilo.com/xyz/still7.png"),
+      (nil, "/still8.png", "https://kinova.igorcamilo.com/xyz/still8.png"),
+    ]
+  )
+  func imagesStillURL(width: Double?, path: StillPath, expectedURL: String) {
+    var images = testImages
+    images.stillSizes = ["abc", "w122", "h123", "w123", "w124", "xyz"]
+    let url = images.url(width: width, path: path)
+    #expect(url?.absoluteString == expectedURL)
+  }
+
+  @Test func imagesStillNoURL() {
+    var images = ConfigurationDetails.Images()
+    images.stillSizes = ["abc"]
+    let url = images.url(path: "still.png" as StillPath)
+    #expect(url == nil)
+  }
+
+  @Test func imagesStillNoPath() {
+    let url = testImages.url(path: "still.svg" as StillPath)
     #expect(url == nil)
   }
 }
